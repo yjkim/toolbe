0ea03cd9810a7d31b39e666b07e5670fdfceb24b
yjkim <yjkim8202@gmail.com>
2014-07-02 21:39:38 +0900

    debug모드

---
+++ xe/config/config.user.inc.php
@@ -0,0 +1,9 @@
+<?php
+        define('__DEBUG__', 1);
+        define('__DEBUG_OUTPUT__', 1);
+        define('__DEBUG_PROTECT__', 0);
+        define('__DEBUG_DB_OUTPUT__', 0);
+        define('__LOG_SLOW_QUERY__', 0);
+        define('__OB_GZHANDLER_ENABLE__', 1);
+        define('__ENABLE_PHPUNIT_TEST__', 0);
+?>
